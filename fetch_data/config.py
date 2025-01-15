cbabff6a34dee94f0df41d9f3eebb7e089207742
Mateusz Wiśniewski <110550176+Vinechetti@users.noreply.github.com>
2025-01-15 21:53:12 +0100

    Add files via upload
    git installation broke, resorting to good old 'by hand approach'

---
+++ fetch_data/config.py
@@ -0,0 +1,5 @@
+OUTPUT_PATH = "data"
+STOCKDATA_NAME = "stock_data.csv"
+TECH_NAME = "indicators.csv"
+CFG_FOLDER = "config"
+CFG_JSON = "config.json"
---
+++ fetch_data/symbols.py
@@ -0,0 +1,12 @@
+# for adding new companies to download data for, add new dict keys to the following dictionary
+# format:
+# COMPANY_NAME: SYMBOl
+
+corp_symbol = {
+    "Apple": "AAPL",
+    "Microsoft": "MSFT",
+    "Amazon": "AMZN",
+    "Alphabet": "GOOG",
+    "Meta Platforms": "META",
+    "S&P500": "^GSPC",
+}
